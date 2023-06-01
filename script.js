90852f06534011e820c809b8116f720832197116
instaosc <oscarowley@live.co.uk>
2023-06-01 16:15:21 -0400

    big commit

--- script.js
+++ script.js
@@ -47,3 +47,53 @@ image.addEventListener("mouseout", function(){
     this.style ="";
     this.width = "100px"
 });
+
+var image = document.getElementById("Simplicity");
+image.addEventListener("mouseover", function(){
+    this.style ="box-shadow: 2px 2px 20px cyan";
+    this.width = "150px"
+});
+var image = document.getElementById("Simplicity");
+image.addEventListener("mouseout", function(){
+    this.style ="";
+    this.width = "100px"
+});
+
+
+
+var image = document.getElementById("Positive");
+image.addEventListener("mouseover", function(){
+    this.style ="box-shadow: 2px 2px 20px cyan";
+    this.width = "150px"
+});
+var image = document.getElementById("Positive");
+image.addEventListener("mouseout", function(){
+    this.style ="";
+    this.width = "100px"
+
+
+
+});var image = document.getElementById("CapeTownView");
+image.addEventListener("mouseover", function(){
+    this.style ="box-shadow: 2px 2px 20px cyan";
+    this.width = "150px"
+});
+var image = document.getElementById("CapeTownView");
+image.addEventListener("mouseout", function(){
+    this.style ="";
+    this.width = "100px"
+});
+
+
+
+var image = document.getElementById("Why");
+image.addEventListener("mouseover", function(){
+    this.style ="box-shadow: 2px 2px 20px cyan";
+    this.width = "150px"
+});
+
+var image = document.getElementById("Why");
+image.addEventListener("mouseout", function(){
+    this.style ="";
+    this.width = "100px"
+});
